eaf439ab577f4bd45c7f796b3eba10c02b54fd5a
makosi-mutua <makosimutua19@gmail.com>
2022-05-24 11:59:00 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,48 +1,48 @@
 #include "lists.h"
-int len(const char *str);
 
 /**
- * *add_node - adds a new node at the beginning of the list.
- * @head: Beginning of the list.
- * @str: String.
- *
- * Return: Address of the new element or NULL if failed.
+ * _strlen - find string length
+ * @str: string
+ * Return: length
  */
-list_t *add_node(list_t **head, const char *str)
+int _strlen(const char *str)
 {
-	list_t *ptr;
+	int len;
 
-	if (head == NULL)
-		return (NULL);
-	ptr = malloc(sizeof(list_t));
-
-	if (ptr == NULL)
-		return (NULL);
-
-	if (*head == NULL)
-		ptr->next = NULL;
-	else
-		ptr->next = *head;
-	ptr->str = strdup(str);
-	ptr->len = len(str);
-	*head = ptr;
-
-	return (*head);
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
 }
 
 /**
- * len - calculates the length of a string.
- * @str: string.
- *
- * Return: The length of the string.
+ * add_node - add node to beginning of linked list
+ * @head: linked list
+ * @str: data for new node
+ * Return: address of new element, or NULL if failed
  */
-int len(const char *str)
-{
-	int i;
 
-	if (str == NULL)
-		return (0);
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node; /* create new node */
+
+	if (str == NULL) /* validate input */
+		return (NULL);
+	if (strdup(str) == NULL) /*check if strdup malloc errored */
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t)); /* malloc for new node */
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str); /* set node values */
+	new_node->len = _strlen(str);
+
+	if (head == NULL) /* set what new node points to first */
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	*head = new_node; /* set head to point to new node */
+
+	return (new_node);
 }
